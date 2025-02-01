14fe42d8376adf1dc3d9f6870b535a408c9dfcda
Liza Chernyshova <liz4chernyshova@yandex.ru>
2025-02-01 13:24:09 +0300

    fix eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -8,10 +8,13 @@ import pluginJs from '@eslint/js';
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended });
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended
+});
 
 export default [
-  { rules: { 'no-console': 'off', 'import/extensions': 'off', 'max-len': ['error', { code: 120 }] } },
+  { rules: { 'no-console': 'off', 'import/extensions': 'off' } },
   { languageOptions: { globals: globals.browser } },
   ...compat.extends('airbnb-base'),
 ];
